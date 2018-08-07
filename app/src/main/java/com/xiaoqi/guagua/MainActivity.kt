4fe0e17e574bb1200c09fb7692b1676d381ac112
Xujie <jie.xu@coer-scnu.org>
2018-08-07 10:54:23 +0800

    添加底部导航UI和侧边导航UI

--- app/src/main/java/com/xiaoqi/guagua/MainActivity.kt
+++ app/src/main/java/com/xiaoqi/guagua/MainActivity.kt
@@ -2,11 +2,24 @@ package com.xiaoqi.guagua
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener
+import android.view.MenuItem
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    }
+
+    private lateinit var mBnv: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initView() {
+        mBnv = findViewById(R.id.bnv_main)
     }
 }
