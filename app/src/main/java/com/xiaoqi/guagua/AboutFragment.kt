b8b8220b1f19e14954f9131856d23dc87d36a7ab
Xu Jie <jiexu215936@sohu-inc.com>
2018-09-05 10:55:44 +0800

    更新LiteItemView版本

--- app/src/main/java/com/xiaoqi/guagua/AboutFragment.kt
+++ app/src/main/java/com/xiaoqi/guagua/AboutFragment.kt
@@ -3,13 +3,15 @@ package com.xiaoqi.guagua
 import android.support.v7.widget.AppCompatTextView
 import android.view.View
 import com.xiaoqi.guagua.util.AppUtil
+import com.xiaoqi.guagua.util.ToastUtil
 import com.xiaoqi.liteitemview.LiteItemView
 
-class AboutFragment : BaseFragment() {
+class AboutFragment : BaseFragment(), LiteItemView.OnLiteItemViewClick {
 
     private lateinit var mTvApp: AppCompatTextView
     private lateinit var mLivUpdate: LiteItemView
     private lateinit var mLivAppreciate: LiteItemView
+    private lateinit var mLivLicense: LiteItemView
 
     companion object {
         fun newInstance(): AboutFragment {
@@ -25,7 +27,19 @@ class AboutFragment : BaseFragment() {
         mTvApp = view.findViewById(R.id.tv_about_app)
         mLivUpdate = view.findViewById(R.id.liv_about_update)
         mLivAppreciate = view.findViewById(R.id.liv_about_appreciate)
+        mLivLicense = view.findViewById(R.id.liv_about_license)
         mLivUpdate.rightText = AppUtil.getVersionCode()
+
+        mLivUpdate.setOnLiteItemViewClick(this)
+        mLivAppreciate.setOnLiteItemViewClick(this)
+        mLivLicense.setOnLiteItemViewClick(this)
     }
 
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.liv_about_update -> {ToastUtil.showMsg((v as LiteItemView).leftText)}
+            R.id.liv_about_appreciate -> {ToastUtil.showMsg((v as LiteItemView).leftText)}
+            R.id.liv_about_license -> {ToastUtil.showMsg((v as LiteItemView).leftText)}
+        }
+    }
 }
