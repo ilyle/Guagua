872c0da6e936cae2c91445e267cbff41fb43942e
Xujie <jie.xu@coer-scnu.org>
2018-10-22 23:04:59 +0800

    文章页头部UI

--- app/src/main/java/com/xiaoqi/guagua/mvp/vp/article/ArticleFragment.kt
+++ app/src/main/java/com/xiaoqi/guagua/mvp/vp/article/ArticleFragment.kt
@@ -1,10 +1,14 @@
 package com.xiaoqi.guagua.mvp.vp.article
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.*
 import android.widget.ImageView
 import com.bumptech.glide.Glide
@@ -22,11 +26,15 @@ import com.xiaoqi.guagua.mvp.vp.article.suggestion.SuggestionFragment
 import com.xiaoqi.guagua.mvp.vp.article.suggestion.SuggestionPresenterImpl
 import com.xiaoqi.guagua.mvp.vp.search.SearchActivity
 
-class ArticleFragment : BaseFragment() {
+class ArticleFragment : BaseFragment(), AppBarLayout.OnOffsetChangedListener {
+    override fun onOffsetChanged(p0: AppBarLayout?, p1: Int) {
+    }
 
     private lateinit var mSuggestionFragment: SuggestionFragment
     private lateinit var mCollectionFragment: CollectionFragment
 
+    private lateinit var mAblArticle: AppBarLayout
+    private lateinit var mCtlArticle: CollapsingToolbarLayout
     private lateinit var mTbArticle: Toolbar
     private lateinit var mIvArticle: ImageView
     private lateinit var mTlArticle: TabLayout
@@ -66,10 +74,15 @@ class ArticleFragment : BaseFragment() {
     }
 
     override fun initView(view: View) {
+        mAblArticle = view.findViewById(R.id.abl_article)
         mTbArticle = view.findViewById(R.id.tb_article)
+        mCtlArticle = view.findViewById(R.id.ctl_article)
         mIvArticle = view.findViewById(R.id.iv_article)
         mTlArticle = view.findViewById(R.id.tl_article)
         mVpArticle = view.findViewById(R.id.vp_article)
+        mAblArticle.addOnOffsetChangedListener(this)
+        mCtlArticle.setCollapsedTitleTextColor(Color.WHITE)
+        mCtlArticle.setExpandedTitleColor(Color.TRANSPARENT)
         mTbArticle.inflateMenu(R.menu.toolbar_main_menu)
         (activity as AppCompatActivity).setSupportActionBar(mTbArticle)
         Glide.with(context!!).load(R.drawable.banner_article).apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.RESOURCE)).into(mIvArticle) // 加载Banner
@@ -77,6 +90,11 @@ class ArticleFragment : BaseFragment() {
         mVpArticle.offscreenPageLimit = 2
         mTlArticle.setupWithViewPager(mVpArticle)
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mAblArticle.removeOnOffsetChangedListener(this)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
