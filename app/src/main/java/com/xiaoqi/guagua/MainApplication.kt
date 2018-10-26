540013131849dd01c37f4b61bd21c6f93014ebc6
Xu Jie <jiexu215936@sohu-inc.com>
2018-10-26 10:56:35 +0800

    加入腾讯Bugly

--- app/src/main/java/com/xiaoqi/guagua/MainApplication.kt
+++ app/src/main/java/com/xiaoqi/guagua/MainApplication.kt
@@ -1,26 +1,41 @@
 package com.xiaoqi.guagua
 
+import android.Manifest
 import android.app.Application
 import android.content.Context
-import android.util.Log
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import com.tencent.bugly.crashreport.CrashReport
 import com.tencent.mmkv.MMKV
-// import com.facebook.stetho.Stetho
+import com.xiaoqi.guagua.constant.Constant
+import com.facebook.stetho.Stetho
 import org.litepal.LitePal
 
 class MainApplication : Application() {
     companion object {
         private lateinit var instance: MainApplication
 
-        @JvmStatic fun getContext() : Context {
+        @JvmStatic
+        fun getContext(): Context {
             return instance.applicationContext
+        }
+
+        fun doAfterPermissionGain() {
+
         }
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
-        LitePal.initialize(this)
-        MMKV.initialize(this@MainApplication)
-        // Stetho.initializeWithDefaults(this);
+        LitePal.initialize(this) // GuoLin的数据库初始化
+        Stetho.initializeWithDefaults(this) // 可以查看本地数据库
+        MMKV.initialize(this@MainApplication) // Tencent的MMKV初始化
+        CrashReport.initCrashReport(this, Constant.BUGLY_APP_ID, true) // Tencent的Bugly初始化
+
+        val gainPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED
+        if (gainPermission) {
+            doAfterPermissionGain()
+        }
     }
 }
--- app/src/main/java/com/xiaoqi/guagua/constant/Constant.kt
+++ app/src/main/java/com/xiaoqi/guagua/constant/Constant.kt
@@ -5,5 +5,10 @@ package com.xiaoqi.guagua.constant
  * 常量
  */
 object Constant {
+
+    const val BUGLY_APP_ID = "9f2bf88006"
+
+    const val BUGLY_APP_KEY = "7d3612c8-855d-49d1-a25a-7a883bd92ffa"
+
     const val SEARCH_HISTORY = "search_history"
 }
